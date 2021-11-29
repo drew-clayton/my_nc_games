642e21c29f82822c1fd07d02970e26ec117b427b
Andrew Clayton <89810052+dro00@users.noreply.github.com>
2021-11-29 17:16:35 +0000

    set up rapp.js, routers, controllers and models for categories endpoint

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -27,7 +27,7 @@ const seed = (data) => {
       .then(() => {
         return db.query(`
       CREATE TABLE categories (
-        slug VARCHAR PRIMARY KEY NOT NULL,
+        slug VARCHAR(50) PRIMARY KEY NOT NULL,
         description TEXT NOT NULL
       );`);
         // creates a new table with name categories
@@ -40,8 +40,8 @@ const seed = (data) => {
       .then(() => {
         return db.query(`
     CREATE TABLE users (
-      username VARCHAR PRIMARY KEY NOT NULL,
-      avatar_url VARCHAR NOT NULL,
+      username VARCHAR(50) PRIMARY KEY NOT NULL,
+      avatar_url VARCHAR(255) NOT NULL,
       name VARCHAR NOT NULL
     );`);
       })
@@ -49,14 +49,14 @@ const seed = (data) => {
         return db.query(`
     CREATE TABLE reviews (
       review_id SERIAL PRIMARY KEY,
-      title VARCHAR NOT NULL,
+      title VARCHAR(255) NOT NULL,
       review_body TEXT NOT NULL,
-      designer VARCHAR NOT NULL,
-      review_img_url VARCHAR DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
-      votes INT DEFAULT 0,
-      category VARCHAR REFERENCES categories(slug) NOT NULL,
-      owner VARCHAR REFERENCES users(username) NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      designer VARCHAR(255) NOT NULL,
+      review_img_url VARCHAR DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg' NOT NULL,
+      votes INT DEFAULT 0 NOT NULL,
+      category VARCHAR(50) REFERENCES categories(slug) ON DELETE CASCADE NOT NULL,
+      owner VARCHAR REFERENCES users(username) ON DELETE CASCADE NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW()
     );`);
         // SERIAL creates unique number after previous number when a row is added (eg 1, 2 ,3, 4, 5)
         // DEFAULT creates a default value
@@ -68,84 +68,80 @@ const seed = (data) => {
         return db.query(`
     CREATE TABLE comments (
       comment_id SERIAL PRIMARY KEY,
-      author VARCHAR REFERENCES users(username) NOT NULL,
-      review_id INT REFERENCES reviews(review_id) NOT NULL,
-      votes INT DEFAULT 0,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      author VARCHAR(50) REFERENCES users(username) ON DELETE CASCADE NOT NULL,
+      review_id INT REFERENCES reviews(review_id) ON DELETE CASCADE NOT NULL,
+      votes INT DEFAULT 0 NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW(),
       body TEXT NOT NULL
     );`);
       })
       // all tables and columns have been created
       //
       .then(() => {
-        const formattedCategories = categoryData.map((obj) => {
-          return [obj.slug, obj.description];
-        });
-        // creates a nested array of all the values of the objects without the keys preparing for %L
-        //
-        // console.log(formattedCategories);
         return db.query(
           format(
             `
         INSERT INTO categories(slug, description)
         VALUES %L`,
-            formattedCategories
+            categoryData.map((obj) => {
+              return [obj.slug, obj.description];
+            })
           )
         );
-        // uses pg-format to add array of values assign them to different rows
+        // categoryData.map((obj) => {return [obj.slug, obj.description]})-----
+        // creates a nested array of all the values of the objects without the keys preparing for %L
+        // uses format() ---
+        // use pg-format to add array of values assign them to different rows
         //
       })
       .then(() => {
-        const formattedUsers = userData.map((obj) => {
-          return [obj.username, obj.name, obj.avatar_url];
-        });
         return db.query(
           format(
             `
         INSERT INTO users(username, name, avatar_url)
         VALUES %L`,
-            formattedUsers
+            userData.map((obj) => {
+              return [obj.username, obj.name, obj.avatar_url];
+            })
           )
         );
       })
       .then(() => {
-        const formattedReviews = reviewData.map((obj) => {
-          return [
-            obj.title,
-            obj.designer,
-            obj.owner,
-            obj.review_img_url,
-            obj.review_body,
-            obj.category,
-            obj.created_at,
-            obj.votes,
-          ];
-        });
         return db.query(
           format(
             `
         INSERT INTO reviews(title, designer, owner, review_img_url, review_body, category, created_at, votes)
         VALUES %L`,
-            formattedReviews
+            reviewData.map((obj) => {
+              return [
+                obj.title,
+                obj.designer,
+                obj.owner,
+                obj.review_img_url,
+                obj.review_body,
+                obj.category,
+                obj.created_at,
+                obj.votes,
+              ];
+            })
           )
         );
       })
       .then(() => {
-        const formattedComments = commentData.map((obj) => {
-          return [
-            obj.body,
-            obj.votes,
-            obj.author,
-            obj.review_id,
-            obj.created_at,
-          ];
-        });
         return db.query(
           format(
             `
         INSERT INTO comments(body, votes, author, review_id, created_at)
         VALUES %L`,
-            formattedComments
+            commentData.map((obj) => {
+              return [
+                obj.body,
+                obj.votes,
+                obj.author,
+                obj.review_id,
+                obj.created_at,
+              ];
+            })
           )
         );
       })
