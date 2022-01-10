8bdee284abc758637322eb7b4374ca5720706abd
Andrew Clayton <89810052+dro00@users.noreply.github.com>
2022-01-10 15:22:36 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const express = require("express");
-// express ...
-
 const apiRouter = require("./routers/api.router");
-// requires apiRouter to begin routing process
-
 const { errorCustom, errorPsql, error500 } = require("./errors");
-// requires in error handling middleware
+const cors = require("cors");
 
 const app = express();
-// creates a variable that references express function which ...
-
+app.use(cors());
 app.use(express.json());
-// makes sure to retrieve body for requests
 
 app.use("/api", apiRouter);
-// first step in the routing using the first function for the first point of route
 
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: `Path Not Found` });
@@ -25,4 +18,3 @@ app.use(errorPsql);
 app.use(error500);
 
 module.exports = app;
-// exports app to be used in tests
